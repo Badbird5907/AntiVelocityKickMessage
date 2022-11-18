45d30dde8c9fb23bb2618fbf266c50bd1f4f71a0
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-18 12:50:08 -0500

    im stupid

--- src/main/java/dev/badbird/velocitykickmessagefix/VelocityKickMessageFix.java
+++ src/main/java/dev/badbird/velocitykickmessagefix/VelocityKickMessageFix.java
@@ -1,13 +1,15 @@
 package dev.badbird.velocitykickmessagefix;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.KickedFromServerEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.slf4j.Logger;
+
+import java.util.Optional;
 
 @Plugin(
         id = "velocitykickmessagefix",
@@ -15,7 +17,6 @@ import org.slf4j.Logger;
         version = BuildConstants.VERSION
 )
 public class VelocityKickMessageFix {
-
     @Inject
     private Logger logger;
 
@@ -26,11 +27,19 @@ public class VelocityKickMessageFix {
     @Subscribe
     public void onPlayerKick(KickedFromServerEvent event) {
         if (event.getResult() instanceof KickedFromServerEvent.DisconnectPlayer result) {
-            Component reason = result.getReasonComponent();
-            if (reason != null) {
-                //logger.info("Reason: " + LegacyComponentSerializer.legacySection().serialize(reason));
-                reason = Component.text("{1}");
-                event.setResult(KickedFromServerEvent.DisconnectPlayer.create(reason));
+            Component reasonComponent = result.getReasonComponent();
+            Optional<Component> optReason = event.getServerKickReason();
+            if (reasonComponent != null && optReason.isPresent()) {
+                /*
+                String reasonString = LegacyComponentSerializer.legacySection().serialize(reasonComponent); // TODO there's probally a better way to do this
+                logger.info("Reason: " + reasonString);
+                if (reasonString.contains("velocity.error.moved-to-new-server")) {
+
+                }
+                event.setResult(KickedFromServerEvent.DisconnectPlayer.create(reasonComponent));
+                 */
+                //wow im stupid, i could have just done the below:
+                event.setResult(KickedFromServerEvent.DisconnectPlayer.create(optReason.get()));
             }
         }
     }
